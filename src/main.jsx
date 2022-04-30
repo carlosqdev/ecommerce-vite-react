9b2f882d384a08e5c52dd1b644923f6ab68f3d71
carlosqdev <carlosquintero95nic@gmail.com>
2022-04-30 17:48:34 -0600

    Styles: global css styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "./main.styl";
 import Router from "./Router";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
