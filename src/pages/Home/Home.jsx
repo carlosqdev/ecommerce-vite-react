ff6a4b386788e3f52b1c89552f13daccfe095352
carlosqdev <carlosquintero95nic@gmail.com>
2022-04-30 17:58:50 -0600

    Feat: using component Header.jsx in page home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from "../../components/Header/Header";
 
 function Home() {
   return (
     <>
-      <div>This is Home</div>
+      <Header />
     </>
   )
 }
